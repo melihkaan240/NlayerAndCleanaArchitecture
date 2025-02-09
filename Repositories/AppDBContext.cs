7f91a4171333ee72559982125fcfdf0f44916c3e
melih kaan araç <melihkaanarac@gmail.com>
2025-02-10 02:26:27 +0300

    first commit

---
+++ Repositories/AppDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{
+    public class AppDBContext(DbContextOptions<AppDBContext> options) : DbContext(options)
+    {
+
+        public DbSet<Product> Products { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
